5598ef70d935421bf767ab8b64cb2ce4805041ca
negi0109 <negi0109.seg@gmail.com>
2021-02-01 01:28:03 +0900

    画像出力

--- maze/dig.rb
+++ maze/dig.rb
@@ -1,15 +1,18 @@
+require './utils/image_converter'
+
 N = 10
-Size = 1 + 2 * N
-Maze = Array.new(Size) do
-    Array.new(Size, false)
+SizeY = 1 + 2 * (N + 1)
+SizeX = 1 + 2 * N
+Maze = Array.new(SizeY) do
+    Array.new(SizeX, false)
 end
 
-def check(x, y)
+def check(y, x)
     rr = []
-    rr.push([1, 0]) if x < Size - 2 && !Maze[x + 2][y]
-    rr.push([0, 1]) if y < Size - 2 && !Maze[x][y + 2]
-    rr.push([-1, 0]) if x > 2 && !Maze[x - 2][y]
-    rr.push([0, -1]) if y > 2 && !Maze[x][y - 2]
+    rr.push([1, 0]) if y < SizeY - 2 && !Maze[y + 2][x]
+    rr.push([0, 1]) if x < SizeX - 2 && !Maze[y][x + 2]
+    rr.push([-1, 0]) if y > 2 && !Maze[y - 2][x]
+    rr.push([0, -1]) if x > 2 && !Maze[y][x - 2]
 
     rr
 end
@@ -25,28 +28,8 @@ end
 
 dig([1, 1])
 
-Maze.each_with_index do |a, i|
-    puts "%2d " % i + a.map{|x| x ? ' ' : '@' }.join
-end
+# Maze.each_with_index do |a, i|
+#     puts "%2d " % i + a.map{|x| x ? ' ' : '@' }.join
+# end
 
-# 0  @@@@@@@@@@@@@@@@@@@@@
-# 1  @     @             @
-# 2  @@@@@ @ @@@@@@@@@@@ @
-# 3  @   @ @   @     @   @
-# 4  @ @@@ @@@ @@@@@ @ @ @
-# 5  @ @   @       @ @ @ @
-# 6  @ @ @@@@@@@@@ @ @ @ @
-# 7  @   @       @   @ @ @
-# 8  @ @@@@@ @@@ @@@ @ @ @
-# 9  @       @ @   @ @ @ @
-# 10 @@@@@@@@@ @@@ @@@ @ @
-# 11 @   @       @   @ @ @
-# 12 @ @ @ @@@@@ @@@ @ @ @
-# 13 @ @ @     @   @ @ @ @
-# 14 @ @ @@@@@ @ @@@ @ @ @
-# 15 @ @       @   @   @ @
-# 16 @ @@@@@@@@@@@ @@@@@ @
-# 17 @     @     @     @ @
-# 18 @@@@@ @@@ @ @@@@@ @ @
-# 19 @         @     @   @
-# 20 @@@@@@@@@@@@@@@@@@@@@
+ImageConverter.array_convert(Maze, { true => 'white',  false => 'black' }, 'maze.dig.png', 5)
